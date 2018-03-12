206fc94e3e78203e0e438b6d99533e3df52c8a7c
kelly <wangyanli0905@126.com>
2018-03-12 11:43:49 +0800

    fix

--- src/page/systemAdmin/index.js
+++
@@ -1,4 +0,0 @@
-export { default as fileManage } from './fileManage.vue'
-export { default as recordManage } from './recordManage.vue'
-export { default as playManage } from './playManage.vue'
-export { default as callManage } from './callManage.vue'
--- src/utils/validate.js
+++
@@ -1,5 +0,0 @@
-/** 2018-1-24
-  * author: Legal High
-  * function: 表单验证
-  */
-// 1、限制input只能输入数字

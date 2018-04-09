73f92dc4dfbc438ae63be56951ae12c87c929279
zjy <1160704409@qq.com>
2018-04-09 10:50:03 +0800

    未提交

---
+++ src/utils/tool.js
@@ -0,0 +1,48 @@
+/** 2018-3-29 0029
+  * author: Legal High
+  * function: 通用工具
+  */
+import vue from 'vue'
+// 是否为数组
+export function isArray(obj)  {
+  if(Array.isArray){
+    return Array.isArray(obj);
+  }else{
+    return Object.prototype.toString.call(obj) === "[object Array]";
+  }
+}
+// 是否为对象
+export function isObject(obj) {
+  return Object.prototype.toString.call(obj) === "[object Object]";
+}
+// 是否为字符串
+export function isString(obj) {
+  return Object.prototype.toString.call(obj) === "[object String]";
+}
+
+// 返回状态
+vue.prototype.returnClass = function(deviceState) {
+  switch (deviceState) {
+    case 'registered':
+      return 'online';
+    case 'ringing':
+      return 'waitting';
+    case 'active':
+      return 'calling'
+    default:
+      return 'online';
+  }
+}
+// 返回class
+vue.prototype.returnState = function(deviceState) {
+  switch (deviceState) {
+    case 'registered':
+      return '在线';
+    case 'ringing':
+      return '振铃';
+    case 'active':
+      return '通话'
+    default:
+      return '在线'
+  }
+}

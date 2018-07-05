a3b305dd7bd5b3758376b7b1c2f93b87c78a48d7
root <root@ieyeplus.ieyeplus.com>
2018-07-05 18:05:52 +0800

    修复管理页高度bug

--- src/utils/page/setting.js
+++ src/utils/page/setting.js
@@ -1,8 +1,8 @@
 import BScroll from 'better-scroll'
 export function getHeights() {
 	//  高度判断
-	  var contentHeight = $(window).height();
-	  var maxHeight = contentHeight - $(".sysName").outerHeight(); 
+	  var windowHeight = $(window).height();
+	  var maxHeight = windowHeight - $(".sysName").outerHeight(); 
 	  var contentHeight = $(".content").outerHeight()
 	  console.log(maxHeight,contentHeight)
 	  if(contentHeight > maxHeight)

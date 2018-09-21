e092f3559148a44547c9f7ebde63e9cd47013aeb
root <root@shyh.scc>
2018-09-21 09:55:11 +0800

    增加了一个文件

---
+++ src/utils/page/device.js
@@ -0,0 +1,18 @@
+export function getHeights() {
+        //  高度判断
+          var windowHeight = $(window).height();
+          var maxHeight = windowHeight - $(".sysName").outerHeight();
+          var contentHeight = $(".table").outerHeight()
+          var popUp = $(".popUp")
+          var popUpHeight = 0
+          if (popUp!=undefined){
+          popUpHeight = popUp.offsetTop + popUp.offsetHeight
+          contentHeight = Math.max( contentHeight,popUpHeight)
+          }
+	  console.log(maxHeight,contentHeight,popUpHeight)
+
+          if(contentHeight > maxHeight)
+        {
+                  $(".content").css('height',contentHeight)
+        }
+}
